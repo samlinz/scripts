730a4d997c368aaa91ed96f96e39a3ca9d50d098
Samuel Lindqvist <samuel.lindqvist@noccela.fi>
2018-06-24 17:56:51 +0300

    add win_logins PS script

---
+++ win_logins/README.md
@@ -0,0 +1,4 @@
+## This script creates CSV from last X Windows logins
+
+The output is CSV file which contains username in left column and login time in the right column
+Argument -size gives the max number of logins fetched, default is 100
---
+++ win_logins/logons.ps1
@@ -0,0 +1,35 @@
+﻿#Requires -RunAsAdministrator
+
+# Create CSV of Windows logons
+
+param([int]$number=100)
+$regex = "(?m)^\s*Account Name:\s+(\S+)\$"
+
+echo "Fetching logons from event log"
+
+$logins = Get-WinEvent -LogName Security -MaxEvents ($number*10) `
+    | Where-Object {$_.Message -match "An account was successfully logged on." } `
+    | Sort-Object {$_.TimeCreated} -Descending `
+    | Select -first $number
+echo "Done"
+
+$login_objs = @()
+
+foreach ($login in $logins) {
+    $matches = $login.Message | Select-String -Pattern $regex
+    $name = $matches.Matches[0].Groups[1].Value
+    $login_objs += New-Object -TypeName psobject -Property @{name=$name; time=$login.TimeCreated}
+}
+
+echo "Creating csv"
+
+$filename = "logins_"
+$date = get-date -format "yyyy-MM-dd-HHmmss"
+
+$root = (Get-Location).Path
+$fullpath = "$root\\$filename$date.csv"
+
+Add-Content -Path $fullpath -Value '"Name","Time"'
+foreach ($login in $login_objs) {
+    $login | Export-Csv -Path $fullpath -Append -NoTypeInformation
+}
